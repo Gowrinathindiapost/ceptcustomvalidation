52a46fe3052067a58c2ca60b025f021d5bf1ed2d
gowrinath <gowrinathindiapost@gmail.com>
2024-09-09 16:24:10 +0530

    09092024 commit3

--- address/address.go
+++ address/address.go
@@ -8,9 +8,11 @@ import (
 
 // Address1 represents a validated address string.
 type Address1 string
+type PhoneNumber1 uint64
 
 // Define a regex for Address1 validation
 var addressPattern = regexp.MustCompile(`^[a-zA-Z0-9 ,.-]{1,255}$`)
+var gValidatePhoneLengthPattern = regexp.MustCompile(`^\d{10}$`)
 
 // Validate checks if the Address1 meets the criteria.
 func (a Address1) Validate() error {
@@ -19,6 +21,15 @@ func (a Address1) Validate() error {
 	}
 	if !addressPattern.MatchString(string(a)) {
 		return errors.New("invalid Address1: contains invalid characters")
+	}
+	return nil
+}
+
+// Validate checks if the phoneNumber meets the criteria (10 digits).
+func (p PhoneNumber1) Validate() error {
+	// Check if the phone number is in the valid range for 10 digits
+	if p < 1000000000 || p > 9999999999 {
+		return errors.New("invalid phoneNumber: must contain exactly 10 digits")
 	}
 	return nil
 }
